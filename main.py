547f6fd15f575dd309b12a951ff63080f5e7c4da
TheGreatGooose <91770855+TheGreatGooose@users.noreply.github.com>
2021-11-21 22:54:48 -0800

    created get_date() function to get submission date

--- main.py
+++ main.py
@@ -21,3 +21,30 @@ def get_boolean(question):
             return False
         else:
             print("Incorrect! Try again...")
+                  
+                  
+def get_date():
+    date_flag = True
+    time_flag = True
+    date_list = []
+    time_list = []
+    while date_flag or time_flag:
+        if date_flag:
+            raw_date = re.sub(r"[^0-9]/","",input("Date of CW submission (dd/mm/yyyy): "))
+            date_list = list(map(int,raw_date.split('/')))
+            try:
+                test_date = datetime.datetime(date_list[2],date_list[1],date_list[0])
+                date_flag = False
+            except ValueError:
+                date_flag = True
+                print("Incorrect date format! Try again...")
+        elif time_flag:
+            raw_time = re.sub(r"[^0-9]:","",input("Time of CW submission (hh:mm): "))
+            time_list = list(map(int,raw_time.split(':')))
+            try:
+                test_time = datetime.datetime(date_list[2],date_list[1],date_list[0], time_list[0],time_list[1])
+                time_flag = False
+            except ValueError:
+                time_flag = True
+                print("Incorrect time format! Try again...")
+    return [date_list[2], date_list[1], date_list[0], time_list[0], time_list[1]]
