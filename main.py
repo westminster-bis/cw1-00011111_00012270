20cfaf6649d5bc8ce6873ec71f5db173f51ffaa1
TheGreatGooose <91770855+TheGreatGooose@users.noreply.github.com>
2021-11-21 22:50:20 -0800

    created function get_mark() to receive mark 0-100

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import re
 import math
 import datetime
+
+def get_mark():
+    while True:
+        n = int(input("Your mark (0-100): "))
+        if n >= 0 and n <= 100:
+            return n
+        else:
+            print("Incorrect! Try again…”)
