f11a4a364ba96f45080d553e46b2374602847e08
wiut12270 <91770773+wiut12270@users.noreply.github.com>
2021-11-30 00:03:09 +0500

    comments and optimizations

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import re
-import math
-import datetime
+#LIBRARIES
+import re 			#Library to edit strings
+import math 		#Library with basic math operations
+import datetime 	#Library to implement date formats
 
+#FUNCTIONS
+#Function to get and edit mark of student
 def get_mark():
     while True:
         n = int(input("Your mark (0-100): "))
@@ -10,19 +13,19 @@ def get_mark():
         else:
             print("Incorrect! Try again…")
 
-
+#Function to get and edit YES/NO input
 def get_boolean(question):
     while True:
         raw_response = input(question).lower()
         response = re.sub(r"[^a-z]","",raw_response)
-        if response == "yes" or response == "true" or response == "y":
+        if response in ["yes", "true", "y"]:
             return True
-        elif response == "no" or response == "false" or response == "n":
+        elif response in ["no", "false", "n"]:
             return False
         else:
             print("Incorrect! Try again...")
                   
-                  
+#Function to get date and time of CW submission        
 def get_date():
     date_flag = True
     time_flag = True
@@ -48,14 +51,16 @@ def get_date():
                 time_flag = True
                 print("Incorrect time format! Try again...")
     return [date_list[2], date_list[1], date_list[0], time_list[0], time_list[1]]
-                  
+
+#Function to get time delay of CW submission  
 def compare_date(d_d,d_s):
     deadline = datetime.datetime(d_d[0],d_d[1],d_d[2],d_d[3],d_d[4])
     submission = datetime.datetime(d_s[0],d_s[1],d_s[2],d_s[3],d_s[4])
     compare_days = (submission-deadline).days 
     compare_days += math.floor((submission-deadline).seconds / 60) / (24 * 60)
     return compare_days
-                  
+
+#Function to print important statements with special outline              
 def final_print(result):
     print()
     print("*"*20)
@@ -63,17 +68,24 @@ def final_print(result):
     print("*"*20)
     print()
 
+
+#MAIN PART
 mark = get_mark()
 date_deadline = [2021, 12, 1, 23, 59]
 print("Deadline for CW submission is 01/12/2021 (December 1, 2021) 23:59!")
 date_submission = get_date()
 compare_days = compare_date(date_deadline, date_submission)
 
+#BOOLEAN LOGIC
 isValid = None
 isAccepted = None
-                  
+
+#MAIN LOGIC
+#Is on time?
 if compare_days <= 0:
     final_print("Great job! You receive " + str(mark) + " (full mark).")
+
+#Within 24 hours?
 elif compare_days <= 1:
     final_print("Late Submission! Your CW was submitted within 24 hours.")
     isValid = get_boolean("Is there a valid reason? Answer YES or NO: ")
@@ -92,7 +104,8 @@ elif compare_days <= 1:
             final_print("Please, submit on time! You receive " + str(mark - 10) + " (-10 from your mark).")
         else:
             final_print("Please, submit on time! You failed to pass this exam (less than 40 and -10 from your mark).")
-   
+
+#Within 5 days?
 elif compare_days <= 5:
     final_print("Late Submission! Your CW was submitted within 5 days.")
     isValid = get_boolean("Is there a valid reason? Answer YES or NO: ")
@@ -106,6 +119,7 @@ elif compare_days <= 5:
     else:
         final_print("Please, submit on time! You failed to pass this exam (0 mark).")
 
+#After 5 days?
 else:
     final_print("No Submission! Your CW was submitted after more than 5 days.")
     isValid = get_boolean("Is there a valid reason? Answer YES or NO: ")
